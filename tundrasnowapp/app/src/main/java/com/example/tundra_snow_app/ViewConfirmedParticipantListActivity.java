076d1756687d11ad2cc4a807316e72906933ca05
Matthew <matthewlai@shaw.ca>
2024-11-04 04:44:50 -0700

    changed look of enrolled participant view

--- tundrasnowapp/app/src/main/java/com/example/tundra_snow_app/ViewConfirmedParticipantListActivity.java
+++ tundrasnowapp/app/src/main/java/com/example/tundra_snow_app/ViewConfirmedParticipantListActivity.java
@@ -27,8 +27,7 @@ public class ViewConfirmedParticipantListActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private ConfirmedListAdapter adapter;
     private String eventID;
-    private Button backButton, editButton, saveButton;
-    private EditText maxParticipantEdit;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +37,13 @@ public class ViewConfirmedParticipantListActivity extends AppCompatActivity {
         participantRecyclerView = findViewById(R.id.waitListBox);
         participantRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        maxParticipantEdit = findViewById(R.id.maxParticipantEdit);
         backButton = findViewById(R.id.backButton);
-        editButton = findViewById(R.id.editButton);
-        saveButton = findViewById(R.id.saveButton);
 
         db = FirebaseFirestore.getInstance();
         eventID = getIntent().getStringExtra("eventID");
 
         loadParticipantList();
         backButton.setOnClickListener(view -> finish());
-        editButton.setOnClickListener(view -> enableEditing(true));
-        saveButton.setOnClickListener(view -> saveEventUpdates());
 
     }
 
@@ -63,40 +57,9 @@ public class ViewConfirmedParticipantListActivity extends AppCompatActivity {
                     participantRecyclerView.setAdapter(adapter);
                 }
 
-                Integer capacity = documentSnapshot.getLong("capacity").intValue();
-                maxParticipantEdit.setText(String.valueOf(capacity));
             }
         });
     }
 
-    private void saveEventUpdates() {
-        // Convert the text from EditText to an integer for maxParticipants
-        int maxParticipantsEdited;
-        try {
-            maxParticipantsEdited = Integer.parseInt(maxParticipantEdit.getText().toString());
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Invalid number for max participants.", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        // Update the user profile in the "users" collection
-        db.collection("events").document(eventID)
-                .update("capacity", maxParticipantsEdited)
-                .addOnSuccessListener(aVoid -> {
-                    Toast.makeText(this, "Event Entry Updated Successfully.", Toast.LENGTH_LONG).show();
-                    enableEditing(false);
-                })
-                .addOnFailureListener(e -> {
-                    Toast.makeText(this, "Failed to update profile.", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-                });
-    }
-
-    private void enableEditing(boolean isEditable) {
-        maxParticipantEdit.setEnabled(isEditable);
-
-        // Toggle visibility of buttons
-        editButton.setVisibility(isEditable ? View.GONE : View.VISIBLE);
-        saveButton.setVisibility(isEditable ? View.VISIBLE : View.GONE);
-    }
 }
